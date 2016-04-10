469be449bb797388869ef61a299f2dba6c892311
windog <windiscall@gmail.com>
2016-04-10 19:23:42 +0800

    SharedPreferences

--- app/src/main/java/com/windog/databasepractice/MainActivity.java
+++ app/src/main/java/com/windog/databasepractice/MainActivity.java
@@ -2,8 +2,12 @@ package com.windog.databasepractice;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -18,6 +22,8 @@ import java.io.OutputStreamWriter;
 public class MainActivity extends Activity {
 
     EditText editText = null;
+    Button savePref;  //保存SharedPreferences
+    Button restore;  //读取SharedPreferences
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +37,34 @@ public class MainActivity extends Activity {
             editText.setSelection(input.length());
             Toast.makeText(MainActivity.this, "success", Toast.LENGTH_LONG).show();
         }
+
+        savePref = (Button) findViewById(R.id.savePrefrences);
+        restore = (Button) findViewById(R.id.restorePrefrences);
+
+        savePref.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = getSharedPreferences("data", MODE_PRIVATE).edit();
+                editor.putString("name", "windy");
+                editor.putInt("age", 28);
+                editor.putBoolean("married", false);
+                editor.commit();
+            }
+        });
+
+        restore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences preferences = getSharedPreferences("data", MODE_PRIVATE);
+                String name = preferences.getString("name", "");
+                int age = preferences.getInt("age", 0);
+                Boolean married = preferences.getBoolean("married", false);
+                Log.d("MainActivity", "name is " + name);
+                Log.d("MainActivity", "age is " + age);
+                Log.d("MainActivity", "married is " + married);
+
+            }
+        });
 
     }
 
