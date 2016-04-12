e0a89f130459b8e5834a8675f1b93cab8dc21b51
windog <windiscall@gmail.com>
2016-04-12 17:40:17 +0800

    SQLiteOpenHelper

---
+++ app/src/main/java/com/windog/databasepractice/app/DbActivity.java
@@ -0,0 +1,17 @@
+package com.windog.databasepractice.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.windog.databasepractice.R;
+
+/**
+ * Created by windog on 2016/4/12.
+ */
+public class DbActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dblayout);
+    }
+}
